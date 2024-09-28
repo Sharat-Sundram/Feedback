7bffaed7c602a9e0dfc244175d50d4c4c9e92054
sharat-sundram <sharat2455@gmail.com>
2024-09-28 15:55:01 +0530

    first commit

---
+++ Thank u Sir.py
@@ -0,0 +1,20 @@
+print("#  Hello, ATUL SIR hope you will be fine .")
+print('            ')
+
+print("""#  I'm Sharat Sundram from CSE(IoT) branch.
+I want to say that this workshop was really very much fruitful for me
+and also this is an interactive and intresting workshop for
+me that I had attended till now.Sir you are very sweet and unique person.
+The special thing of yours is that you never
+take the credits for the good works you had done.
+In workshops, i generally saw that
+trainee start lagging in their works due to
+less interaction by mentor but in this workshop because of your sweet nature
+no one is afraid or hesitate of asking their
+problems and this is the reason that
+everyone standing at the same lavel,you answer the questions
+with much clarity every time irrespect of time it is asked by u.
+once again thankyou so much sir.You are one of the best mentor.""")
+
+print('        ')
+print("#   Thankyou very much ATUL SIR...")
---
+++ practice.py
@@ -0,0 +1,24 @@
+s="sundram","Rajesh"
+l=['s','u','n','d','r','a','m']
+print(list(l))
+
+print(type(s))
+
+s2={2,3,4,"sundram"}
+print(s2.add("sharat"))
+print(s2)
+
+n=int(input("Enter a value:"))
+
+if n>=100 and n<=2000:
+    print("The number lie betwwen the given range:")
+
+else:
+    print("The number is out of range")
+
+
+
+num1=int(input("Enter first number"))
+num2=int(input("Enter second number"))
+
+print("the sum is",num1+num2)
